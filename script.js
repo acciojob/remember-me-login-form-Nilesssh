f74fd3fefab5bd8e8872956bf759cee276441d15
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-06-21 16:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+let checkbox = document.querySelector("#checkbox");
+let name = document.querySelector("#username");
+let password = document.querySelector("#password");
+checkbox.addEventListener("change",function(){
+    if(this.checked){
+        if(name && password){
+            localStorage.setItem("name",username.value);
+            localStorage.setItem("password",password.value);
+        }else{
+            alert("Fill the Empty Fields");
+        }
+    }else{
+        localStorage.removeItem("name");
+        localStorage.removeItem("password");
+    }
+})
